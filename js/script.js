79c7ac8f5712ed6d333d508eb7d028eba1cfd13f
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-08-01 09:15:54 +0300

    update script.js

--- js/script.js
+++ js/script.js
@@ -8,16 +8,55 @@ function getPizza( name, size, crust, topping, total ) {
     this.size = size;
     this.crust = crust;
     this.topping = topping;
-    this.total = total;
+    this.total = totalPrice;
 }
 
 
 //switch size
 function pSize(size){
     switch(size){
-        case
+        case "0":
+            price = 0
+        break;
+        case "small":
+            price = 750
+        break;
+        case "medium":
+            price = 950
+        break;
+        case "large":
+            price = 1200
+        break;
+        default:
+            console.log("No price");
+    
     }
+    return price
 }
+
+//switch crust
+
+function pCrust(crust){
+    switch(crust){
+        case "0":
+            price = 0
+        break;
+        case "crispy":
+            price = 150
+        break;
+        case "gluten-free":
+            price = 250
+        break;
+        case "stuffed":
+            price = 200
+        break;
+        default:
+            console.log("No price");
+    
+    }
+    return price
+}
+
 
 
 
@@ -42,12 +81,23 @@ $(document).ready(function() {
     $("button.addToCart").click(function(event) {
         event.preventDefault();
         let pizzaName = $(".pizzaName option:selected").val();
-        let pizzaSize = $(".pizzaSize option:selected").val();
-        let pizzaCrust = $(".pizzaCrust option:selected").val();
+        let size = $(".pizzaSize option:selected").val();
+        let crust = $(".pizzaCrust option:selected").val();
         let pizzaToppings =[];
         $.each($("input[name = 'pizzaTopping']:checked"), function(){
             pizzaToppings.push($(this).val());
         });
-        console.log(pToppings);
+        
+
+
+        pizzaSize = pSize(size);
+        pizzaCrust = pCrust(crust);
+        alert(pizzaToppings)
+        alert(pizzaSize);
+        alert(pizzaCrust);
+
+
+
+
     });
 });

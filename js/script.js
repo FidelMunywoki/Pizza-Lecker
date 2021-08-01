d96b004bb1de54a057ba8bbab38790b053ac9ffb
FidelMunywoki <fidelis.munywoki@student.moringaschool.com>
2021-08-01 22:50:13 +0300

    update script.js

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ function getPizza( name, size, crust, topping, total ) {
     this.crust = crust;
     this.topping = topping;
     this.total = total;
+    
 }
 
 
@@ -68,7 +69,15 @@ function totalPizzaPrice(sizePrice,crustPrice,totalToppingPrice) {
     return sizePrice + crustPrice + totalToppingPrice;
 }
 
+//prototype for price of home delivery
 
+function homeDelivery(homeDeliveryCheckout) {
+    this.homeDeliveryCheckout = homeDeliveryCheckout;
+}
+
+homeDelivery.prototype.addDeliveryCost = function () {
+    return this.homeDeliveryCheckout + 200
+}
 
 
 
@@ -119,12 +128,6 @@ $(document).ready(function() {
         pizzaTotalPrice = totalPizzaPrice(pizzaSizePrice, pizzaCrustPrice, pizzaToppingPrice);
         let checkoutTotalPrice = 0;
         checkoutTotalPrice = checkoutTotalPrice + pizzaTotalPrice;
-
-
-        // alert(pizzaSizePrice);
-        // alert(pizzaCrustPrice);
-        // alert(pizzaToppingPrice);
-        // alert(pizzaTotalPrice);
         
 
         //post to cart
@@ -155,10 +158,101 @@ $(document).ready(function() {
 
             //post new pizza to table
            
-            $("#customer-orders").append('<tr><td id="pizza-name">'+newPizza.name +'</td><td id="pizza-size">' + newPizza.size + '</td><td id="pizza-crust">'+newPizza.crust + '</td><td id="pizza-topping">'+newPizza.topping+'</td><td id="all-total">'+newPizza.total+'</td></tr>');
-            alert(newPizza);
+            $(".pizzaTable").append('<tr><td id="pizza-name">'+newPizza.name +'</td><td id="pizza-size">' + newPizza.size + " @"+pizzaSizePrice +'</td><td id="pizza-crust">'+newPizza.crust +  " @"+pizzaCrustPrice + '</td><td id="pizza-topping">'+newPizza.topping+  " @"+pizzaToppingPrice +'</td><td id="all-total">'+ "Ksh. "+newPizza.total+'</td></tr>');
+            
 
         });
+
+
+        //Checkout Button
+        $("button.checkout").click(function() { 
+            $("button.checkout").hide();
+            $("button.extra-pizza").hide();
+            $("button.home-delivery").slideDown(1000);
+            $("button.place-order").slideDown(1000);
+            $("#extra-price").slideDown(1000);
+
+            //TO ADD COMPLETE ORDER
+            $(".pizzaTable").append('<tr><td id="all-total">'+"Total Ksh. "+checkoutTotalPrice +'</td></tr>'); 
+            $("#total-pizza").append("Your Order bill is Ksh. " +checkoutTotalPrice);
+        });
+
+        //place order normal
+         $("button.place-order").click(function() {
+            $(".pizzaTable").hide();
+            $(".customer-choice").hide();
+            $('.extra-price').hide();
+            $("total-pizza").hide();
+            $("#normal-order").append("Your Total bill is: "+ checkoutTotalPrice+
+            "<p>Thank for shopping with us Kindly collect your order at Nairobi CBD Tall park within 8hrs</p>"+
+            "<p>You can still make another order to stand a chance of winning a free PIZZA TREAT DAY!!</p>");
+            $(".addToCart").slideDown(1000);
+            
+
+            //empty contents
+            checkoutTotalPrice = "";
+            $(".pizzaName option:selected").val("");
+            $(".pizzaSize option:selected").val("");
+            $(".pizzaCrust option:selected").val("");
+            pizzaToppings.splice(0, pizzaToppings.length);
+           
+
+           
+
+            
+        });
+
+
+        //home delivery button
+        $("button.home-delivery").click(function() {
+            $(".pizzaTable").hide();
+            $(".customer-choice").hide();
+            $('.extra-price').hide();
+            $(".delivery-infor").slideDown(1000);
+            $("total-pizza").hide();
+            $("#total-bill").slideDown(1000);
+            finalPrice = new homeDelivery(checkoutTotalPrice);
+            $("#total-bill").append("Your Total bill plus delivery fee is: "+ finalPrice.addDeliveryCost());
+           
+
+            
+        });
+
+        //place order button for delivery
+        $("button.place-order-home").click(function(event) {
+            event.preventDefault();
+            let personName = $("input#your-name").val();
+            let email =$("input#email").val();
+            let phoneNumber =$("input#phoneNumber").val();
+            let location =$("input#location").val();
+
+            if (personName && phoneNumber && email && location != ""){
+                $("#total-bill").hide();
+                swal({
+                    title: "Sent!",
+                    text: personName+"Thank you for shopping with us. We have received your Order and it will be delivered in 1 hour",
+                    icon: "success",
+                    button: "OK!",
+                  });
+                  $("#ack-message").append("Hello "+personName+" You can still continue shopping with us for offers and give ways")
+
+                  $("input#your-name").val("");
+                  $("input#email").val("");
+                  $("input#phoneNumber").val("");
+                  $("input#location").val("");
+
+                  $("#delivery-infor").hide();
+                  
+                  $("#ack-message").slideDown(1000);
+
+
+            }
+            else {
+                alert("Please fill in the delivery!");
+
+            }
+        });
+
 
 
 
